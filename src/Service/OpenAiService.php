21b91542d79a3ba2ed130e5907c966d5bb6bc923
Jessica Kuijer <jessicakuijer@me.com>
2023-03-16 12:51:18 +0100

    🔨 refactor(OpenAiService.php): refactor OpenAiService class The OpenAiService class has been refactored to improve readability and maintainability. The constructor now has a private property for the ParameterBagInterface. The getStory method has been refactored to use a match expression instead of an if-else statement. The variable names have been changed to use camelCase instead of snake_case.
    🔨 refactor(OpenAiService.php): simplify return statement
    The return statement in the if block has been simplified to return the value directly instead of assigning it to a variable first. The else block has also been simplified to return the error message directly instead of assigning it to a variable first.

--- src/Service/OpenAiService.php
+++ src/Service/OpenAiService.php
@@ -7,25 +7,25 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class OpenAiService
 {
-    public function __construct(ParameterBagInterface $parameterBag) {
+    private ParameterBagInterface $parameterBag;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
         $this->parameterBag = $parameterBag;
     }
 
     public function getStory(string $story, string $type = 'history'): string
     {
-        $open_ai_key = $this->parameterBag->get('OPENAI_API_KEY');
-        
-        $open_ai = new OpenAi($open_ai_key);
+        $openAiKey = $this->parameterBag->get('OPENAI_API_KEY');
+        $openAi = new OpenAi($openAiKey);
 
-        if ($type === 'alternative') {
-            $prompt = 'Raconte moi une histoire pour enfants avec une leçon de vie à la fin et avec les éléments suivants: ' . $story;
-        } elseif ($type === 'scary') {
-            $prompt = 'Raconte moi une histoire très effrayante pour enfants avec les éléments suivants: ' . $story;
-        } else {
-            $prompt = 'Raconte moi une histoire pour enfants avec des rebondissements incroyables et avec les éléments suivants: ' . $story;
-        }
+        $prompt = match ($type) {
+            'alternative' => 'Raconte moi une histoire pour enfants avec une leçon de vie à la fin et avec les éléments suivants: ' . $story,
+            'scary' => 'Raconte moi une histoire très effrayante pour enfants avec les éléments suivants: ' . $story,
+            default => 'Raconte moi une histoire pour enfants avec des rebondissements incroyables et avec les éléments suivants: ' . $story,
+        };
 
-        $complete = $open_ai->completion([
+        $complete = $openAi->completion([
             'model' => 'text-davinci-003',
             'prompt' => $prompt,
             'temperature' => 0,
@@ -37,10 +37,9 @@ class OpenAiService
         $json = json_decode($complete, true);
 
         if (isset($json['choices'][0]['text'])) {
-            $json = $json['choices'][0]['text'];
-            return $json;
+            return $json['choices'][0]['text'];
         }
-        $json = 'Une erreur est survenue. Je n\'ai pas pu vous aider et ne peux créer cette histoire pour l\'instant.';
-        return $json;
+
+        return 'Une erreur est survenue. Je n\'ai pas pu vous aider et ne peux créer cette histoire pour l\'instant.';
     }
 }
